dffa1e97c398a7e9646cb9184ee582297853b587
Umesh Sahani <umeshsahani1998@gmail.com>
2025-02-14 17:47:16 +0530

    Update tinymce with advanced part

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,8 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>NoteApp - laravel 11</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -15,19 +14,75 @@
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
+  
     <script src="https://cdn.jsdelivr.net/npm/tinymce@6/tinymce.min.js"></script>
     <script>
         tinymce.init({
             selector: '#blog_content',
-            height: 400,
-            plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
-            toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | image | help',
+            height: 500,
+            plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount imagetools media code',
+            toolbar: 'undo redo | styles | formatselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | image table media code | removeformat | help',
             branding: false,
 
             // Enable automatic image upload
             images_upload_url: '{{ route('upload.image') }}',
             automatic_uploads: true,
+            image_advtab: true,
+            image_caption: true,
+            image_title: true,
+
+            // Additional image styling options
+            image_class_list: [{
+                    title: 'None',
+                    value: ''
+                },
+                {
+                    title: 'Responsive',
+                    value: 'img-fluid'
+                },
+                {
+                    title: 'Rounded',
+                    value: 'rounded'
+                },
+                {
+                    title: 'Bordered',
+                    value: 'border'
+                },
+                {
+                    title: 'Shadow',
+                    value: 'shadow'
+                }
+            ],
+
+            // Table enhancements
+            table_advtab: true,
+            table_default_styles: {
+                width: '100%',
+                borderCollapse: 'collapse'
+            },
+            table_class_list: [{
+                    title: 'None',
+                    value: ''
+                },
+                {
+                    title: 'Striped',
+                    value: 'table-striped'
+                },
+                {
+                    title: 'Bordered',
+                    value: 'table-bordered'
+                },
+                {
+                    title: 'Hover',
+                    value: 'table-hover'
+                }
+            ],
+
+            // Allow inserting media (videos, audio, iframe embeds)
+            media_live_embeds: true,
+
+            // Allow code formatting
+            code_dialog_width: 600,
 
             // Handle image selection from local files
             file_picker_callback: function(callback, value, meta) {
@@ -59,7 +114,6 @@
             }
         });
     </script>
-
 
 
 
