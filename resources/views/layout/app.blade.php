e8c6fc159e04cfdc558d985768c34b753b2c44be
Umesh Sahani <umeshsahani1998@gmail.com>
2025-02-15 11:00:22 +0530

    Add body color

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body>
+<body style="background: #e2e2e2">
 
     @yield('content')
 
